5b6b391c19379eba5644676633aa3484789684d5
aashiish0106 <aashish.pandey.cg@gmail.com>
2026-08-26 02:56:59 +0530

    python folder added and html assignment worked up

---
+++ python/02.1_variables_practice_problems/assignment.py
@@ -0,0 +1,77 @@
+#18
+name = "Rahul"
+age = 18
+city = "Patna"
+print(name)
+print(age)
+print(city)
+#19
+age = 18
+age = 19
+print(age)
+#final value will be 19 because the value will update
+#20
+name = "Rahul"
+name = "Amit"
+name = "Riya"
+print(name)
+#final value will be Riya 
+#21
+student_name = "Rahul"
+student_age = 18
+
+student_age = 19
+print(student_name)
+print(student_age)
+
+#this will give the student name Rahul and the age 19 because the value is updated
+#22
+a, b, c = 10, 20, 30
+print(a,b,c)
+
+#this will assign the a as 10 , b as 20 and c as 30
+#23
+x = y = z = 100
+print(x,y,z)
+#x ,y and z have the value 100
+
+#24
+my_name="Aashiish"
+my_age=18
+my_city="Lucknow"
+
+#25
+student_name="Unnatii"
+student_roll=20042008
+student_branch="Btech CSE AI"
+
+#26
+marks=90
+marks=80
+print(marks)
+
+#27
+name , age , city = "Rahul" , 18 , "Patna"
+print(name,age,city)
+
+#28
+x=y=z=0
+print(x,y,z)
+
+#29
+# 1student = "Rahul"
+# student name = "Rahul"
+# class = "B.Tech"
+#below givwn are the correct variable naming 
+student1="Rahul"
+student_name="Rahul"
+student_class="B.Tech"
+
+#30
+student_name = "Rahul"
+student_age = 18
+
+student_age = 19
+print(student_name)
+print(student_age)
+
---
+++ python/03.1_data_types_practice_problems/assignment.py
@@ -0,0 +1,95 @@
+#18
+x = 25
+print(type(x))
+#this will give int
+
+#19
+price = 99.50
+print(type(price))
+#this will give float
+
+#20
+name = "Python"
+print(type(name))
+#this will give str
+
+#21
+is_active = True
+print(type(is_active))
+#this will give bool
+
+#22
+data = None
+print(type(data))
+#this will give None type 
+
+#23
+name="Aashii"
+age=18
+weight=48.7
+is_student=True
+marks=None
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(is_student))
+print(type(marks))
+
+#24
+student_name="Aashish"
+student_age=18
+student_height=178.9
+is_student=True
+student_result=None
+print(type(student_name))
+print(type(student_age))
+print(type(student_height))
+print(type(is_student))
+print(type(student_result))
+
+#25
+a=50
+print(type(a))#int
+a=50.0
+print(type(a))#float
+a="50"
+print(type(a))#bool
+
+#26
+a=True
+print(type(a))#this is a bool type
+b="True"
+print(type(b))#this is a str type because it is in " "
+
+#27
+a=None
+print(type(a))#this is a None type
+b="None"
+print(type(b))#this is a str type because it is in " "
+
+#28
+var=18
+print(type(var))
+var="18"
+print(type(var))
+
+#"" will be str 
+a=None
+print(type(a))#this is a None type
+b="None"
+print(type(b))#this is a str type because it is in " "
+
+a=True
+print(type(a))#this is a bool type
+b="True"
+print(type(b))#this is a str type because it is in " "
+
+a=10
+print(type(a))
+b=10.0
+print(type(b))
+c="10"
+print(type(c))
+
+
+
