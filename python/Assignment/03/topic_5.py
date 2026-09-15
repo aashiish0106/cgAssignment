f20ae57e62e1adc8cee23e8b6344c6ad1c30cd6e
aashiish0106 <aashish.pandey.cg@gmail.com>
2026-09-15 21:14:46 +0530

    topic 5 in 03 python done

--- python/Assignment/03/topic_5.py
+++ python/Assignment/03/topic_5.py
@@ -1,1 +1,50 @@
 # Topic-5 — String Indexing
+
+# Q19. Basic Indexing
+
+text = "PYTHON"
+# First character
+print(text[0])
+
+# Second character
+print(text[1])
+
+# Last character
+print(text[-1])
+
+# Second-last character
+print(text[-2])
+
+# Q20. Positive and Negative Indexing
+
+text = "COMPUTER"
+
+print(text[0])
+print(text[3])
+print(text[-1])
+print(text[-3])
+
+# Q21. Predict the Output
+text = "PYTHON"
+
+print(text[0])#P
+print(text[2])#T
+print(text[-1])#N
+print(text[-2])#O
+
+# Q22. Indexing a User Input
+
+word=input("Enter a word:")
+print("First character:",word[0])
+print("Last character:",word[-1])
+
+# Q23. Think Carefully About Indexing
+
+word = "PROGRAM"
+
+print(word[0])#P
+print(word[2])#O
+print(word[-1])#M
+print(word[-4])#G
+
+
